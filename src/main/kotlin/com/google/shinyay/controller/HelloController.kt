9f57c77360d922f23635be78f663e6ee99a11bbb
shinyay <shinya.com@gmail.com>
2021-01-26 22:09:34 +0900

    Add REST function for GET Mapping

--- src/main/kotlin/com/google/shinyay/controller/HelloController.kt
+++ src/main/kotlin/com/google/shinyay/controller/HelloController.kt
@@ -1,9 +1,15 @@
 package com.google.shinyay.controller
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1")
 class HelloController {
+
+    @GetMapping
+    fun hello() {
+        
+    }
 }
