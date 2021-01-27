7afc86876a5333e108e235580cda67bcb41a760f
shinyay <shinya.com@gmail.com>
2021-01-27 23:10:20 +0900

    Add return type as String

--- src/main/kotlin/com/google/shinyay/controller/HelloController.kt
+++ src/main/kotlin/com/google/shinyay/controller/HelloController.kt
@@ -13,9 +13,10 @@ import java.time.format.DateTimeFormatter
 class HelloController {
 
     @GetMapping("hello")
-    fun hello() {
+    fun hello(): String? {
         val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
         val currentTime = ZonedDateTime.now(ZoneId.of("Japan")).format(dateFormat)
         logger.info("Current Time: $currentTime")
+        return currentTime
     }
 }
